97af96276999e9120581be034a97568d7f18b93d
KogaIJ <150610187+KogaIJ@users.noreply.github.com>
2023-11-12 19:09:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const repoOwner = 'KogaIJ';
-const repoName = 'titan-s-bravery';
-const folderPath = '/items';
+function selectRandomItem() {
+  const repoOwner = 'KogaIJ';
+  const repoName = 'titan-s-bravery';
+  const folderPath = '/items';
 
-fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${folderPath}`)
-  .then(response => response.json())
-  .then(data => {
-    const items = data.map(item => item.name);
+  fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${folderPath}`)
+    .then(response => response.json())
+    .then(data => {
+      const items = data.map(item => item.name);
 
-    const randomIndex = Math.floor(Math.random() * items.length);
-    const randomItem = items[randomIndex];
+      const randomIndex = Math.floor(Math.random() * items.length);
+      const randomItem = items[randomIndex];
 
-    document.getElementById('randomItem').textContent = randomItem;
-    document.getElementById('randomImage').src = folderPath + '/' + randomItem;
-  })
-  .catch(error => {
-    console.error('Failed to fetch icons:', error);
-  });
+      document.getElementById('randomItem').textContent = randomItem;
+      document.getElementById('randomImage').src = `https://raw.githubusercontent.com/${repoOwner}/${repoName}/main${folderPath}/${randomItem}`;
+    })
+    .catch(error => {
+      console.error('Failed to fetch icons:', error);
+    });
+}
